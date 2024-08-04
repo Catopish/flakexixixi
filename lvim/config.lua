0acc04438c1bf0a1d41e67fdb1682c17ec5a0e86
al <athila2001@gmail.com>
2024-08-04 13:58:04 +0700

    percobaan

--- lvim/config.lua
+++ lvim/config.lua
@@ -13,4 +13,19 @@ lvim.plugins= { -- Lua
   {
     "Jezda1337/nvim-html-css",
   },
+  {
+  "goolord/alpha-nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
+  "derektata/lorem.nvim",
+  keys = { { "<leader>1", "<cmd>LoremIpsum 1 paragraphs<cr>", desc = "Lorem" } },
+  config = function()
+    require("lorem").setup({
+      sentenceLength = "medium",
+      comma_chance = 0.2,
+      max_commas_per_sentence = 2,
+    })
+  end,
+  },
 }
