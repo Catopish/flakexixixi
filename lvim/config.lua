14531b526715d3c8acf97f1a6b82be8c106e286b
Al <athila2001@gmail.com>
2024-08-16 14:34:17 +0700

    Naisu

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,4 +1,15 @@
 vim.opt.relativenumber = true
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    command = "prettier",
+    filetypes = { "javascript", "typescript", "css", "scss", "json", "html", "markdown" },
+  },
+}
+
+-- Enable format on save
+lvim.format_on_save = true
+
 lvim.plugins= { -- Lua
   {
     "folke/twilight.nvim",
@@ -13,10 +24,12 @@ lvim.plugins= { -- Lua
   {
     "Jezda1337/nvim-html-css",
   },
+
   {
   "goolord/alpha-nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   },
+
   {
   "derektata/lorem.nvim",
   keys = { { "<leader>1", "<cmd>LoremIpsum 1 paragraphs<cr>", desc = "Lorem" } },
@@ -27,5 +40,21 @@ lvim.plugins= { -- Lua
       max_commas_per_sentence = 2,
     })
   end,
+  },
+
+  {
+    'goolord/alpha-nvim',
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+        'nvim-lua/plenary.nvim'
+    },
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.theta'.config)
+    end
+  },
+
+  {
+  'stevearc/conform.nvim',
+  opts = {},
   },
 }
